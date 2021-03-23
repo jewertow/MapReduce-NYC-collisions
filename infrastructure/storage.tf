3b0adb3132aac4e86ce6a325a9e44a8df7c01527
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2021-03-23 21:30:06 +0100

    Upload test input data

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -5,10 +5,10 @@ resource "google_storage_bucket" "google_storage_bucket_primary" {
   provider      = google
 }
 
-resource "google_storage_bucket_object" "google_storage_bucket_object_mapreduce_input_1" {
-  name   = "mapreduce/input/datasource1.txt"
+resource "google_storage_bucket_object" "google_storage_bucket_object_test_mapreduce_input_1" {
+  name   = "mapreduce/input/test/ds1.csv"
   bucket = google_storage_bucket.google_storage_bucket_primary.name
-  source = "${var.mapreduce_input_location}/ds1.txt"
+  source = "${var.mapreduce_input_location}/datasource1/${var.mapreduce_input_ds1}_test.csv"
 }
 
 resource "google_storage_bucket_object" "google_storage_bucket_object_mapreduce_input_2" {
