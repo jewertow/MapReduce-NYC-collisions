493a6cb0361d1f0f0a69ba7688a810b49926e60a
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2021-03-24 01:07:20 +0100

    Submit hadoop job

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -11,13 +11,13 @@ resource "google_storage_bucket_object" "collisions_mapreduce_job_jar" {
   source = "${var.project_location}/${var.mapreduce_job_jar_location}"
 }
 
-resource "google_storage_bucket_object" "test_mapreduce_input_1" {
+resource "google_storage_bucket_object" "mapreduce_test_input_1" {
   name   = "mapreduce/input/test/ds1.csv"
   bucket = google_storage_bucket.primary.name
   source = "${var.project_location}/input/datasource1/${var.mapreduce_input_ds1}_test.csv"
 }
 
-resource "google_storage_bucket_object" "test_mapreduce_input_2" {
+resource "google_storage_bucket_object" "mapreduce_test_input_2" {
   name   = "mapreduce/input/datasource2.txt"
   bucket = google_storage_bucket.primary.name
   source = "${var.project_location}/input/ds2.txt"
