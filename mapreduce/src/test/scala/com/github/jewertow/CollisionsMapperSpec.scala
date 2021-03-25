9f462f2120f6ace61aa103ed628a72d2cdcecc74
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2021-03-25 21:52:58 +0100

    Refactor

--- mapreduce/src/test/scala/com/github/jewertow/CollisionsMapperSpec.scala
+++ mapreduce/src/test/scala/com/github/jewertow/CollisionsMapperSpec.scala
@@ -1,30 +1,25 @@
 package com.github.jewertow
 
+import com.github.jewertow.HadoopExt._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.FlatSpec
-import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
+import org.apache.hadoop.io.LongWritable
 import org.mockito.Mockito._
 
 class CollisionsMapperSpec extends FlatSpec with MockitoSugar {
 
   private val mapper = new CollisionsMapper
+  private val key = new LongWritable(1)
 
   it should "return single result record when collision has only one type of participants" in {
     // given
     val context = mock[mapper.Context]
 
     // when
-    mapper.map(
-      key = new LongWritable(1),
-      value = new Text("08/03/2013,18:00,11223,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0"),
-      context
-    )
+    mapper.map(key, value = "08/03/2013,18:00,11223,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0".text, context)
 
     // then
-    verify(context).write(
-      new Text("37 AVENUE,11223,pedestrians,injured"),
-      new IntWritable(2)
-    )
+    verify(context).write("37 AVENUE,11223,pedestrians,injured".text, 2.writable)
     verifyNoMoreInteractions(context)
   }
 
@@ -33,25 +28,12 @@ class CollisionsMapperSpec extends FlatSpec with MockitoSugar {
     val context = mock[mapper.Context]
 
     // when
-    mapper.map(
-      key = new LongWritable(1),
-      value = new Text("08/03/2013,18:00,11223,,,,37 AVENUE,,,,4,0,2,0,1,0,1,0"),
-      context
-    )
+    mapper.map(key, value = "08/03/2013,18:00,11223,,,,37 AVENUE,,,,4,0,2,0,1,0,1,0".text, context)
 
     // then
-    verify(context).write(
-      new Text("37 AVENUE,11223,pedestrians,injured"),
-      new IntWritable(2)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,cyclist,injured"),
-      new IntWritable(1)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,motorist,injured"),
-      new IntWritable(1)
-    )
+    verify(context).write("37 AVENUE,11223,pedestrians,injured".text, 2.writable)
+    verify(context).write("37 AVENUE,11223,cyclist,injured".text, 1.writable)
+    verify(context).write("37 AVENUE,11223,motorist,injured".text, 1.writable)
     verifyNoMoreInteractions(context)
   }
 
@@ -60,37 +42,15 @@ class CollisionsMapperSpec extends FlatSpec with MockitoSugar {
     val context = mock[mapper.Context]
 
     // when
-    mapper.map(
-      key = new LongWritable(1),
-      value = new Text("08/03/2013,18:00,11223,,,,37 AVENUE,,,,4,3,2,1,1,1,1,1"),
-      context
-    )
+    mapper.map(key, value = "08/03/2013,18:00,11223,,,,37 AVENUE,,,,4,3,2,1,1,1,1,1".text, context)
 
     // then
-    verify(context).write(
-      new Text("37 AVENUE,11223,pedestrians,injured"),
-      new IntWritable(2)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,pedestrians,killed"),
-      new IntWritable(1)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,cyclist,injured"),
-      new IntWritable(1)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,cyclist,killed"),
-      new IntWritable(1)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,motorist,injured"),
-      new IntWritable(1)
-    )
-    verify(context).write(
-      new Text("37 AVENUE,11223,motorist,killed"),
-      new IntWritable(1)
-    )
+    verify(context).write("37 AVENUE,11223,pedestrians,injured".text, 2.writable)
+    verify(context).write("37 AVENUE,11223,pedestrians,killed".text, 1.writable)
+    verify(context).write("37 AVENUE,11223,cyclist,injured".text, 1.writable)
+    verify(context).write("37 AVENUE,11223,cyclist,killed".text, 1.writable)
+    verify(context).write("37 AVENUE,11223,motorist,injured".text, 1.writable)
+    verify(context).write("37 AVENUE,11223,motorist,killed".text, 1.writable)
     verifyNoMoreInteractions(context)
   }
 
@@ -99,11 +59,7 @@ class CollisionsMapperSpec extends FlatSpec with MockitoSugar {
     val context = mock[mapper.Context]
 
     // when
-    mapper.map(
-      key = new LongWritable(1),
-      value = new Text("08/03/2012,18:00,11223,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0"),
-      context
-    )
+    mapper.map(key, value = "08/03/2012,18:00,11223,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0".text, context)
 
     // then
     verifyNoMoreInteractions(context)
@@ -116,11 +72,7 @@ class CollisionsMapperSpec extends FlatSpec with MockitoSugar {
 
     // when
     invalidZipCodes.foreach { zipCode =>
-      mapper.map(
-        key = new LongWritable(1),
-        value = new Text(s"08/03/2012,18:00,$zipCode,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0"),
-        context
-      )
+      mapper.map(key, value = s"08/03/2012,18:00,$zipCode,,,,37 AVENUE,,,,2,0,2,0,0,0,0,0".text, context)
     }
 
     // then
