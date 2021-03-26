5090d2d7dc38d2bc564629ae954dc3a27812355a
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2021-03-26 23:29:11 +0100

    Provision Hive job

--- infrastructure/dataproc.tf
+++ infrastructure/dataproc.tf
@@ -1,3 +1,20 @@
+locals {
+  hive_drop_old_table          = "drop table if exists collisions"
+  hive_create_table_collisions = <<EOF
+    create external table collisions(
+      street string,
+      zip_code string,
+      participant string,
+      injury string,
+      participants_number int
+    )
+    row format
+    delimited fields terminated by ","
+    stored as textfile
+    location 'gs://${google_storage_bucket.primary.name}/mapreduce/output/${var.execution_date_time}';
+EOF
+}
+
 resource "google_dataproc_cluster" "mapreduce_cluster" {
   name     = var.dataproc_mapreduce_cluster_name
   region   = var.region
@@ -47,7 +64,7 @@ resource "google_dataproc_job" "hadoop_collisions_mapreduce_job" {
     main_jar_file_uri = "gs://${google_storage_bucket.primary.name}/${google_storage_bucket_object.collisions_mapreduce_job_jar.name}"
     args = [
       "gs://${google_storage_bucket.primary.name}/${google_storage_bucket_object.collisions_dataset.name}",
-      "gs://${google_storage_bucket.primary.name}/mapreduce/output/${formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())}"
+      "gs://${google_storage_bucket.primary.name}/mapreduce/output/${var.execution_date_time}"
     ]
   }
 
@@ -60,4 +77,26 @@ resource "google_dataproc_job" "hadoop_collisions_mapreduce_job" {
 
 output "hadoop_collisions_mapreduce_job_status" {
   value = google_dataproc_job.hadoop_collisions_mapreduce_job.status[0].state
+}
+
+resource "google_dataproc_job" "hive_collisions_db" {
+  region = google_dataproc_cluster.mapreduce_cluster.region
+
+  placement {
+    cluster_name = google_dataproc_cluster.mapreduce_cluster.name
+  }
+
+  hive_config {
+    query_list = [
+      local.hive_drop_old_table,
+      local.hive_create_table_collisions,
+      "select * from collisions limit 10;",
+      "select count(*) from collisions;",
+    ]
+  }
+
+  depends_on = [
+    google_dataproc_cluster.mapreduce_cluster,
+    google_dataproc_job.hadoop_collisions_mapreduce_job
+  ]
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,6 +14,10 @@ variable "zone" {
   default = "a"
 }
 
+variable "execution_date_time" {
+  description = "env variable that stores UNIX timestamp"
+}
+
 variable "dataproc_mapreduce_cluster_name" {
   default = "mapreduce-cluster"
 }
